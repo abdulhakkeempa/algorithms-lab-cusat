ecf68decb3af70ae4e4e63215a20834eff9d2a67
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-01-16 15:05:11 +0530

    knapsack problem source code and output uploaded

--- Lab Cycle 3/Knapsack.cpp
+++ Lab Cycle 3/Knapsack.cpp
@@ -1,10 +1,6 @@
-/* A Naive recursive implementation of
-0-1 Knapsack problem */
 #include <bits/stdc++.h>
 using namespace std;
 
-// A utility function that returns
-// maximum of two integers
 int max(int a, int b) { return (a > b) ? a : b; }
 
 // Returns the maximum value that
